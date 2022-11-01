36fb92c29f3d87a3c4fd3f86eadb3ab8fbcc269b
Christian Valenti <christian.valenti@uni-ulm.de>
2022-11-01 21:11:30 +0100

    Daily LC balanced binary tree :libra: :tanabata_tree:

---
+++ src/leetcode/0110_balanced_binary_tree.rs
@@ -0,0 +1,50 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::cmp;
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+  pub val: i32,
+  pub left: Option<Rc<RefCell<TreeNode>>>,
+  pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+  #[inline]
+  pub fn new(val: i32) -> Self {
+    TreeNode {
+      val,
+      left: None,
+      right: None
+    }
+  }
+}
+
+fn main() {
+
+}
+
+pub fn is_balanced(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+    is_balanced_ref(&root)
+}
+
+fn get_height(root: &Option<Rc<RefCell<TreeNode>>>) -> i32 {
+    match root {
+        None => 0,
+        Some(tree) => {
+            1 + cmp::max(get_height(&tree.borrow().left), get_height(&tree.borrow().right))
+        }
+    }
+}
+
+fn is_balanced_ref(root: &Option<Rc<RefCell<TreeNode>>>) -> bool {
+    match root {
+        None => true,
+        Some(tree) => {
+            (get_height(&tree.borrow().left) - get_height(&tree.borrow().right)).abs() <= 1
+                && is_balanced_ref(&tree.borrow().left)
+                && is_balanced_ref(&tree.borrow().right)
+        }
+    }
+}
