e67a1ee975cae2da5acba68b13268b2f961c0eda
Christian D. Valenti <christian.valenti@uni-ulm.de>
2023-01-08 19:24:05 +0100

    Improved DLC using foldl equivalent to solve in one line :file_folder: :japanese_goblin:

--- src/leetcode/0136_single_number.py
+++ src/leetcode/0136_single_number.py
@@ -1,3 +1,7 @@
+import functools
+import operator
+
+
 def single_number(nums: list[int]) -> int:
     answer = 0
     for i in nums:
@@ -5,6 +9,14 @@ def single_number(nums: list[int]) -> int:
     return answer
 
 
+def single_number_one_liner(nums: list[int]) -> int:
+    return functools.reduce(operator.xor, nums, 0)
+
+
 print(single_number([2, 2, 1]))
 print(single_number([4, 1, 2, 1, 2]))
 print(single_number([1]))
+
+print(single_number_one_liner([2, 2, 1]))
+print(single_number_one_liner([4, 1, 2, 1, 2]))
+print(single_number_one_liner([1]))
