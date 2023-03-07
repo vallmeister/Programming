5faed3bc15c08e7bb093b61385c1b6cbc74ce793
Christian D. Valenti <christian.valenti@uni-ulm.de>
2023-03-07 19:09:16 +0100

    Easy LC for the streak :baby_chick:

---
+++ src/leetcode/0027_remove_element.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        n = len(nums)
+        i = 0
+        while i < n:
+            if nums[i] == val:
+                nums[i] = nums[n - 1]
+                n -= 1
+            else:
+                i += 1
+        return i
+
+
+s = Solution()
+print(s.removeElement([3, 2, 2, 3], 3))
+print(s.removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2))
