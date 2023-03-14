a7ec62a5ab4cbcb9490bbfe55a1c5fe39c7aa235
Christian D. Valenti <christian.valenti@uni-ulm.de>
2023-03-14 09:14:32 +0100

    DP only safe values if you need i+1 and i+2 at the same time :eight:

--- src/leetcode/0309_best_time_to_buy_and_sell_stock_with_cooldown.py
+++ src/leetcode/0309_best_time_to_buy_and_sell_stock_with_cooldown.py
@@ -1,6 +1,4 @@
 class Solution:
-    memo = {}
-
     def maxProfit(self, prices: list[int]) -> int:
         n = len(prices)
         buy_after_sell = 0  # dp[1][i + 2]
@@ -8,7 +6,7 @@ class Solution:
         sell = prices[n - 1]  # dp[0][i + 1]
         curr_buy = 0
 
-        for i in range(n - 2, -1 , -1):
+        for i in range(n - 2, -1, -1):
             curr_sell = max(sell, buy_after_sell + prices[i])
             curr_buy = max(buy, sell - prices[i])
             buy_after_sell = buy
@@ -28,22 +26,24 @@ class Solution:
         return dp[1][0]
 
     # return self.max_profit_memo(prices, 0, True)
-    def max_profit_memo(self, prices: list[int], idx: int, can_buy: bool) -> int:
+    def max_profit_memo(self, prices: list[int], idx: int, can_buy: bool, memo: dict) -> int:
         n = len(prices)
         if idx >= n:
             return 0
-        elif can_buy and idx >= n - 2:
+        elif can_buy and idx >= n - 1:
             return 0
+        elif (can_buy, idx) in memo:
+            return memo[(can_buy, idx)]
         elif can_buy:
-            self.memo[(True, idx)] = max(self.max_profit_memo(prices, idx + 1, True),
-                                         self.max_profit_memo(prices, idx + 1, False)
-                                         - prices[idx])
-            return self.memo[(True, idx)]
+            memo[(True, idx)] = max(self.max_profit_memo(prices, idx + 1, True, memo),
+                                    self.max_profit_memo(prices, idx + 1, False, memo)
+                                    - prices[idx])
+            return memo[(True, idx)]
         else:
-            self.memo[(False, idx)] = max(self.max_profit_memo(prices, idx + 1, False),
-                                          self.max_profit_memo(prices, idx + 2, True)
-                                          + prices[idx])
-            return self.memo[(False, idx)]
+            memo[(False, idx)] = max(self.max_profit_memo(prices, idx + 1, False, memo),
+                                     self.max_profit_memo(prices, idx + 2, True, memo)
+                                     + prices[idx])
+            return memo[(False, idx)]
 
 
 s = Solution()
@@ -55,3 +55,10 @@ print(s.maxProfit([7, 6, 4, 3, 1]))
 print(s.maxProfit(
     [48, 12, 60, 93, 97, 42, 25, 64, 17, 56, 85, 93, 9, 48, 52, 42, 58, 85, 81, 84, 69, 36, 1, 54, 23, 15, 72, 15, 11,
      94]))
+
+print(s.max_profit_memo([1, 2, 3, 0, 2], 0, True, {}))
+print(s.max_profit_memo([1], 0, True, {}))
+print(s.max_profit_memo([7, 1, 5, 3, 6, 4], 0, True, {}))
+print(s.max_profit_memo(
+    [48, 12, 60, 93, 97, 42, 25, 64, 17, 56, 85, 93, 9, 48, 52, 42, 58, 85, 81, 84, 69, 36, 1, 54, 23, 15, 72, 15, 11,
+     94], 0, True, {}))
