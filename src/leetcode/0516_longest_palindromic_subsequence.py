04b9b846f8e1b2f39ea57ba8b41b7800136ac0e8
Christian D. Valenti <christian.valenti@uni-ulm.de>
2023-03-21 20:50:02 +0100

    DP understood subsequence problem :star:

--- src/leetcode/0516_longest_palindromic_subsequence.py
+++ src/leetcode/0516_longest_palindromic_subsequence.py
@@ -1,34 +1,52 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        return self.longest_palindrome_subseq_dp(s)
+        n = len(s)
+        dp = [1, 0]
+        for j in range(1, n):
+            new_row = [0] * (j + 1)
+            for i in range(j, -1, -1):
+                if i == j:
+                    new_row[i] = 1
+                elif s[i] == s[j]:
+                    new_row[i] = 2 + dp[i + 1]
+                else:
+                    new_row[i] = max(new_row[i + 1], dp[i])
+            dp = new_row
+            dp.append(0)  # avoid index out of list for 2nd case
+        return dp[0]
 
     def longest_palindrome_subseq_dp(self, s):
         n = len(s)
-        if s == s[::-1]:
-            return n
-        dp = [[0] * 2 for _ in range(n + 1)]
-        
-        print(dp)
-        return max(dp[0])
+        dp = [[0] * n for _ in range(n)]
+        for j in range(n):
+            for i in range(j, -1, -1):
+                if i == j:
+                    dp[i][j] = 1
+                elif s[i] == s[j]:
+                    dp[i][j] = 2 + dp[i + 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+        return dp[0][n - 1]
 
     def longest_palindrome_subseq_memo(self, s):
         memo = {}
 
-        def longest_palindrome_subseq(s1, i):
-            n = len(s1)
-            if s1 == s1[::-1]:
-                return n
-            elif (s1, i) in memo:
-                return memo[(s1, i)]
-            elif i >= n:
-                memo[(s1, i)] = 0
-                return memo[(s1, i)]
+        def longest_palindrome_subseq(i, j):
+            if i > j:
+                return 0
+            elif (i, j) in memo:
+                return memo[(i, j)]
+            elif i == j:
+                memo[(i, j)] = 1
+                return memo[(i, j)]
+            elif s[i] == s[j]:
+                memo[(i, j)] = 2 + longest_palindrome_subseq(i + 1, j - 1)
+                return memo[(i, j)]
             else:
-                memo[(s1, i)] = max(longest_palindrome_subseq(s1, i + 1),
-                                    longest_palindrome_subseq(s1[:i] + s1[i + 1:], i))
-                return memo[(s1, i)]
+                memo[(i, j)] = max(longest_palindrome_subseq(i + 1, j), longest_palindrome_subseq(i, j - 1))
+                return memo[(i, j)]
 
-        return longest_palindrome_subseq(s, 0)
+        return longest_palindrome_subseq(0, len(s) - 1)
 
 
 sol = Solution()
