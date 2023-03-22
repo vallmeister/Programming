194ac121d99a805687ee3902a26f40c29e6e40da
Christian D. Valenti <christian.valenti@uni-ulm.de>
2023-03-22 09:22:55 +0100

    Easy parenthesis :leopard:

---
+++ src/leetcode/0022_generate_parantheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> list[str]:
+        parenthesis = []
+
+        def generate(curr_parenthesis, left_open, left_closed):
+            if left_open == 0 and left_closed == 0:
+                parenthesis.append(curr_parenthesis)
+            elif left_open == 0:
+                generate(curr_parenthesis + ')', left_open, left_closed - 1)
+            elif left_open == left_closed:
+                generate(curr_parenthesis + '(', left_open - 1, left_closed)
+            else:
+                generate(curr_parenthesis + '(', left_open - 1, left_closed)
+                generate(curr_parenthesis + ')', left_open, left_closed - 1)
+
+        start = '('
+        generate(start, n - 1, n)
+        return parenthesis
+
+
+s = Solution()
+print(s.generateParenthesis(3))
+print(s.generateParenthesis(1))
