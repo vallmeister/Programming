2f14966e66b5913ca391fb2a1446050718f3fe01
Christian Valenti <christian.valenti@uni-ulm.de>
2023-08-25 19:14:33 +0200

    DLC DP :construction:

---
+++ src/leetcode/0097_interleaving_strings.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        m = len(s1)
+        n = len(s2)
+        if m + n != len(s3):
+            return False
+        dp = [[False] * (m + 1) for _ in range(n + 1)]
+        for i in range(m + 1):
+            dp[0][i] = True
+        for i in range(n + 1):
+            dp[i][0] = True
+        
+
+
+s = Solution()
+print(s.isInterleave("aabcc", s2="dbbca", s3="aadbbcbcac"))
+print(s.isInterleave("aabcc", s2="dbbca", s3="aadbbbaccc"))
+print(s.isInterleave('', '', ''))
