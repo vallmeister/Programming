137b9f162c886f66a1b69b17d0d4715c844a5d82
Christian Valenti <christian.valenti@uni-ulm.de>
2024-01-09 21:19:21 +0100

    DLC  easy and learned about generator :nine: :four:

---
+++ src/leetcode/0872_leaf_similar_trees.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        sequence_1 = []
+        sequence_2 = []
+
+        def get_sequence(node, seq):
+            stack = [node]
+            while stack:
+                node = stack.pop()
+                if not node:
+                    continue
+                if not node.left and not node.right:
+                    seq.append(node.val)
+                else:
+                    stack.append(node.right)
+                    stack.append(node.left)
+
+        get_sequence(root1, sequence_1)
+        get_sequence(root2, sequence_2)
+        return len(sequence_1) == len(sequence_2) and all(m == n for m, n in zip(sequence_1, sequence_2))
