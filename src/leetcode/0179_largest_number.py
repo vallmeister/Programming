93c2dbbf8633234b69e6b6a0dc32824010a00dd0
Christian Valenti <christian.valenti@uni-ulm.de>
2024-01-27 15:00:23 +0100

    DLC hard :flushed: :100: :one: :two:

---
+++ src/leetcode/0179_largest_number.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        pass
+
+
+s = Solution()
+print(s.largestNumber([10, 2]))
+print(s.largestNumber([3, 30, 34, 5, 9]))
---
+++ src/leetcode/0629_k_inverse_pairs_array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def kInversePairs(self, n: int, k: int) -> int:
+        pass
+
+
+s = Solution()
+print(s.kInversePairs(3, 0))
+print(s.kInversePairs(3, 1))
