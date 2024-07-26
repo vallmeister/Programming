07feae9eb88230883302b70292d07d65efa210d6
Christian Valenti <christian.valenti@uni-ulm.de>
2024-07-26 09:39:24 +0200

    Remember Floyd-Warshall: k-i-j :exclamation: :one: :zero:

--- src/leetcode/1334_find_city_with_smallest_number_of_neighbors_at_threshold_distance.py
+++ src/leetcode/1334_find_city_with_smallest_number_of_neighbors_at_threshold_distance.py
@@ -1,34 +1,32 @@
 import math
-from heapq import heappop, heappush
 from typing import List
 
 
 class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
-        adjacency_list = [[] for _ in range(n)]
+        graph = [[math.inf] * n for _ in range(n)]
         for source, target, weight in edges:
-            adjacency_list[source].append([target, weight])
-            adjacency_list[target].append([source, weight])
-
-        shortest_paths = [[math.inf] * n for _ in range(n)]
-        for source in range(n):
-            distances = [math.inf] * n
-            pq = [(0, source)]
-            while pq:
-                d, node = heappop(pq)
-                if d < distances[node]:
-                    distances[node] = d
-                    for neighbor, distance in adjacency_list[node]:
-                        heappush(pq, (distance + d, neighbor))
-            shortest_paths[source] = distances
-
-        city, count = -1, n + 1
+            graph[source][target] = weight
+            graph[target][source] = weight
         for i in range(n):
-            curr = 0
-            for j in range(n):
-                if shortest_paths[i][j] <= distanceThreshold:
-                    curr += 1
-            if curr <= count:
+            graph[i][i] = 0
+
+        # all nodes shortest paths with Floyd-Warshall
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+        least_neighbors = math.inf
+        city = -1
+        for i in range(n - 1, -1, -1):
+            reachable_neighbors = sum(1 if distance <= distanceThreshold else 0 for distance in graph[i])
+            if reachable_neighbors < least_neighbors:
+                least_neighbors = reachable_neighbors
                 city = i
-                count = curr
         return city
+
+
+s = Solution()
+print(s.findTheCity(n=4, edges=[[0, 1, 3], [1, 2, 1], [1, 3, 4], [2, 3, 1]], distanceThreshold=4))
+print(s.findTheCity(6, [[0, 1, 10], [0, 2, 1], [2, 3, 1], [1, 3, 1], [1, 4, 1], [4, 5, 10]], 20))
