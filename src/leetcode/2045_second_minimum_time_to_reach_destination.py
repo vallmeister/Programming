f4197c19b514d0dbd9ff25d27e3a89d25254e94c
Christian Valenti <christian.valenti@uni-ulm.de>
2024-07-28 21:32:01 +0200

    Very hard :worried: :one: :three:

---
+++ src/leetcode/2045_second_minimum_time_to_reach_destination.py
@@ -0,0 +1,13 @@
+import math
+from heapq import heappop, heappush
+from typing import List
+
+
+class Solution:
+    def secondMinimum(self, n: int, edges: List[List[int]], time: int, change: int) -> int:
+        pass
+
+
+s = Solution()
+print(s.secondMinimum(n=5, edges=[[1, 2], [1, 3], [1, 4], [3, 4], [4, 5]], time=3, change=5))
+print(s.secondMinimum(n=2, edges=[[1, 2]], time=3, change=2))
