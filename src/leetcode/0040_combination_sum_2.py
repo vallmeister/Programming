4b25bc2f4babbf7c19057716e57c4bfba03f459d
Christian Valenti <christian.valenti@uni-ulm.de>
2024-08-13 11:01:23 +0200

    Tricky backtracking to avoid duplicates :astonished: :two: :nine:

--- src/leetcode/0040_combination_sum_2.py
+++ src/leetcode/0040_combination_sum_2.py
@@ -3,27 +3,24 @@ from typing import List
 
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        ans = set()
         candidates.sort()
+        ans = []
+        self.backtracking(0, candidates, target, [], ans)
+        return ans
 
-        def backtrack(start, curr_nums, curr_sum):
-            if curr_sum == target:
-                ans.add(tuple(curr_nums))
-                return
-            elif curr_sum > target:
-                return
-            next_numbers = set()
-            for i in range(start, len(candidates)):
-                num = candidates[i]
-                if num in next_numbers:
-                    continue
-                curr_nums.append(num)
-                backtrack(i + 1, curr_nums, curr_sum + num)
-                curr_nums.pop()
-                next_numbers.add(num)
-
-        backtrack(0, [], 0)
-        return [list(tpl) for tpl in ans]
+    def backtracking(self, i, candidates, target, curr_nums, ans):
+        if target < 0:
+            return
+        elif target == 0:
+            ans.append(list(curr_nums))
+            return
+        for j in range(i, len(candidates)):
+            if j > i and candidates[j] == candidates[j - 1]:
+                continue
+            num = candidates[j]
+            curr_nums.append(num)
+            self.backtracking(j + 1, candidates, target - num, curr_nums, ans)
+            curr_nums.pop()
 
 
 s = Solution()
