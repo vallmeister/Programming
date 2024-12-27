450c6c341274500ffbc8d95905a26200f79b6e56
Christian Valenti <christian.valenti97@gmail.com>
2024-12-27 11:24:27 +0100

    DLC :100: :six: :five:

--- src/leetcode/1014_best_sightseeing_pair.py
+++ src/leetcode/1014_best_sightseeing_pair.py
@@ -1,13 +1,20 @@
 class Solution:
     def maxScoreSightseeingPair(self, values: list[int]) -> int:
-        curr = 0
-        result = 0
-        for val in values:
-            result = max(result, curr + val)
-            curr = max(curr, val) - 1
-        return result
+        n = len(values)
+        val_j = [0] * n
+        for j in range(n):
+            val_j[j] = values[j] - j
+        max_after = [0] * n
+        max_after[-1] = -n
+        for i in reversed(range(n - 1)):
+            max_after[i] = max(max_after[i + 1], val_j[i + 1])
+        ans = 0
+        for i in range(n - 1):
+            ans = max(ans, values[i] + i + max_after[i])
+        return ans
 
 
 s = Solution()
 print(s.maxScoreSightseeingPair([8, 1, 5, 2, 6]))
 print(s.maxScoreSightseeingPair([1, 2]))
+print(s.maxScoreSightseeingPair([1, 1, 1]))
